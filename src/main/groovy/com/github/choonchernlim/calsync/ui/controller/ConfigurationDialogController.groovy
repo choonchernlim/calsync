ec5abe42e0fcdf2fa0a295db28aaeb624d915a2a
Choon-Chern Lim <meteoric@rocketmail.com>
2016-12-19 21:11:11 -0600

    dialog controller - exchange

--- src/main/groovy/com/github/choonchernlim/calsync/ui/controller/ConfigurationDialogController.groovy
+++ src/main/groovy/com/github/choonchernlim/calsync/ui/controller/ConfigurationDialogController.groovy
@@ -1,16 +1,36 @@
 package com.github.choonchernlim.calsync.ui.controller
 
-import javafx.event.ActionEvent
 import javafx.fxml.FXML
+import javafx.fxml.Initializable
+import javafx.scene.control.ChoiceBox
+import javafx.scene.control.Control
 import javafx.scene.control.TextField
 import javafx.stage.FileChooser
 
-final class ConfigurationDialogController {
+final class ConfigurationDialogController implements Initializable {
+    private static final String FORM_FIELD_ERROR_STYLE = 'form-field-error'
+
+    @FXML
+    ChoiceBox<String> exchangeUserEnv
+
+    @FXML
+    ChoiceBox<String> exchangePasswordEnv
+
     @FXML
     TextField clientSecretFile
 
+    @Override
+    void initialize(final URL location, final ResourceBundle resources) {
+        def envs = System.getenv().keySet().sort()
+
+        exchangeUserEnv.items.addAll(envs)
+        exchangePasswordEnv.items.addAll(envs)
+
+        addErrorStyleClass(exchangeUserEnv, exchangePasswordEnv)
+    }
+
     @SuppressWarnings("GrMethodMayBeStatic")
-    void handleClientSecretFileChooser(ActionEvent actionEvent) {
+    void handleClientSecretFileChooser() {
         final FileChooser fileChooser = new FileChooser(initialDirectory: new File(System.getProperty('user.home')))
         fileChooser.extensionFilters.add(new FileChooser.ExtensionFilter('JSON', '*.json'))
 
@@ -23,5 +43,25 @@ final class ConfigurationDialogController {
         else {
             println 'canceled'
         }
+    }
+
+    void onExchangeUserEnvAction() {
+        removeErrorStyleClass(exchangeUserEnv)
+    }
+
+    void onExchangePasswordEnvAction() {
+        removeErrorStyleClass(exchangePasswordEnv)
+    }
+
+    private static void addErrorStyleClass(Control... formFields) {
+        assert formFields
+
+        formFields.each { it.styleClass.add(FORM_FIELD_ERROR_STYLE) }
+    }
+
+    private static void removeErrorStyleClass(Control... formFields) {
+        assert formFields
+
+        formFields.each { it.styleClass.remove(FORM_FIELD_ERROR_STYLE) }
     }
 }
